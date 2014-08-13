039ed4366da81ead38140787adba4b6186fe070d
pgrm4tL <jrsf2208@gmail.com>
2014-08-12 20:26:09 -0500

    my second commit

---
+++ MyRProject/src/MyRProject/MyRProject.java
@@ -0,0 +1,18 @@
+package MyRProject;
+
+public class MyRProject {
+	
+	public static void main(String [] args) {
+		
+		Recipies one = new Recipies();
+		one.setRecipeName("MyFirstRecipe");
+		one.setRecipeNumber(1);
+		
+		Recipies two = new Recipies();
+		two.setRecipeName("MySecondRecipe");
+		two.setRecipeNumber(2);
+		
+		System.out.println(one.getRecipeName());
+		System.out.println(two.getRecipeName());
+	}
+}
---
+++ MyRProject/src/MyRProject/Recipies.java
@@ -0,0 +1,24 @@
+package MyRProject;
+
+public class Recipies {
+	int recipeNumber = 0;
+	String recipeName = "";
+	String[] recipeIngrediants;
+	
+	public void setRecipeNumber(int n){
+		recipeNumber = n;
+	}
+	public int getRecipeNumber() {
+		return recipeNumber;
+	}
+	
+	public void setRecipeName(String name){
+		recipeName = name;
+	}
+	public String getRecipeName() {
+		return recipeName;
+	}
+	
+	
+
+}
